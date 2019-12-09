0a6d3dd20a46484d308dfa77babc6d36abbdeab3
Your_Name <joseph161216@gmail.com>
2019-12-09 14:15:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// const input = document.querySelector("input")
 function addingEventListener() {
-}
+  const input = document.getElementById("input");
+  input.addEventListener("click", function(event) {
+    alert("I was clicked!");
+  });
+};
+
+// input.addEventListener("click", addingEventListener)
